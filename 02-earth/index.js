33826427abbb35049b07fec3822147d95afb21d7
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-07-14 21:44:02 +0200

    add starfield

--- 02-earth/index.js
+++ 02-earth/index.js
@@ -1,9 +1,12 @@
 import * as THREE from "three";
 import { OrbitControls } from "jsm/controls/OrbitControls.js"
+import getStarfield from "./src/getStarfield.js";
+
 console.log(`THREE REVISION: %c${THREE.REVISION}`,"color: #FFFF00")
 window.THREE = THREE;
 const w = window.innerWidth;
 const h = window.innerHeight;
+const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.z = 3;
 const renderer = new THREE.WebGLRenderer({ antialias: true});
@@ -12,6 +15,7 @@ document.body.appendChild(renderer.domElement);
 
 const earthGroup = new THREE.Group();
 earthGroup.rotation.z = -23.4 * Math.PI / 180;
+scene.add(earthGroup)
 new OrbitControls(camera, renderer.domElement);
 const loader = new THREE.TextureLoader();
 const geometry = new THREE.IcosahedronGeometry(1, 12);
@@ -21,13 +25,16 @@ const material = new THREE.MeshPhongMaterial({
 const earthMesh = new THREE.Mesh(geometry, material);
 earthGroup.add(earthMesh);
 
+const stars = getStarfield({ numStars: 2000 });
+scene.add(stars)
+
 const hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000)
 earthGroup.add(hemiLight);
 function animate() {
   requestAnimationFrame(animate);
 
   earthMesh.rotation.y += 0.002;
-  renderer.render(earthGroup, camera);
+  renderer.render(scene, camera);
 }
 
 animate();
