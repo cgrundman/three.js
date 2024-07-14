e7ba8779337d107cb23199065cdcc1c820b7f160
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-07-14 21:55:43 +0200

    add lights

--- 02-earth/index.js
+++ 02-earth/index.js
@@ -15,25 +15,38 @@ document.body.appendChild(renderer.domElement);
 
 const earthGroup = new THREE.Group();
 earthGroup.rotation.z = -23.4 * Math.PI / 180;
+const detail = 4;
 scene.add(earthGroup)
 new OrbitControls(camera, renderer.domElement);
 const loader = new THREE.TextureLoader();
-const geometry = new THREE.IcosahedronGeometry(1, 12);
+const geometry = new THREE.IcosahedronGeometry(1, detail);
 const material = new THREE.MeshPhongMaterial({
   map: loader.load("textures/earthmap1k.jpg")
 });
 const earthMesh = new THREE.Mesh(geometry, material);
 earthGroup.add(earthMesh);
 
+const lightsMat = new THREE.MeshBasicMaterial({
+  map: loader.load("textures/earthlights1k.jpg"),
+  blending: THREE.AdditiveBlending,
+})
+const lightsMesh = new THREE.Mesh(geometry, lightsMat)
+earthGroup.add(lightsMesh)
+
+
 const stars = getStarfield({ numStars: 2000 });
 scene.add(stars)
 
-const hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000)
-earthGroup.add(hemiLight);
+// const hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000)
+// scene.add(hemiLight);
+const sunLight = new THREE.DirectionalLight(0xffffff)
+scene.add(sunLight)
+sunLight.position.set(-2, 0.5, 1.5)
 function animate() {
   requestAnimationFrame(animate);
 
   earthMesh.rotation.y += 0.002;
+  lightsMesh.rotation.y += 0.002;
   renderer.render(scene, camera);
 }
 
