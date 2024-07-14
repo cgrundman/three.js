a174447f78cdb0c23c1b6403e2f77eb5b852d483
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-07-14 21:36:49 +0200

    rotate earth rotation axis

--- 02-earth/index.js
+++ 02-earth/index.js
@@ -1,17 +1,17 @@
 import * as THREE from "three";
 import { OrbitControls } from "jsm/controls/OrbitControls.js"
-
 console.log(`THREE REVISION: %c${THREE.REVISION}`,"color: #FFFF00")
 window.THREE = THREE;
 const w = window.innerWidth;
 const h = window.innerHeight;
-const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
-camera.position.z = 5;
+camera.position.z = 3;
 const renderer = new THREE.WebGLRenderer({ antialias: true});
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 
+const earthGroup = new THREE.Group();
+earthGroup.rotation.z = -23.4 * Math.PI / 180;
 new OrbitControls(camera, renderer.domElement);
 const loader = new THREE.TextureLoader();
 const geometry = new THREE.IcosahedronGeometry(1, 12);
@@ -19,16 +19,15 @@ const material = new THREE.MeshPhongMaterial({
   map: loader.load("textures/earthmap1k.jpg")
 });
 const earthMesh = new THREE.Mesh(geometry, material);
-scene.add(earthMesh);
+earthGroup.add(earthMesh);
 
 const hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000)
-scene.add(hemiLight);
+earthGroup.add(hemiLight);
 function animate() {
   requestAnimationFrame(animate);
 
-  earthMesh.rotation.x += 0.001;
   earthMesh.rotation.y += 0.002;
-  renderer.render(scene, camera);
+  renderer.render(earthGroup, camera);
 }
 
 animate();
