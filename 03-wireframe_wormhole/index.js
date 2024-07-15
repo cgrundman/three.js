0c9a817e09ded8e9b2b1b2357adb1d277b16282d
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-07-15 16:51:08 +0200

    create line from spline function

--- 03-wireframe_wormhole/index.js
+++ 03-wireframe_wormhole/index.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "jsm/controls/OrbitControls.js";
+import spline from "./spline.js";
 
 const w = window.innerWidth;
 const h = window.innerHeight;
@@ -8,26 +9,25 @@ const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(w, h);
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.outputColorSpace = THREE.SRGBColorSpace;
 document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.03;
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshStandardMaterial({
-  color: 0xffff00,
-});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const points = spline.getPoints(100);
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
+const material = new THREE.LineBasicMaterial({ color: 0xffffff });
+const line = new THREE.Line(geometry, material);
+scene.add(line)
 
 const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
 scene.add(hemiLight);
 
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.02;
   renderer.render(scene, camera);
   controls.update();
 }
